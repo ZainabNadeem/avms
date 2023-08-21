4d8af140740322981f4891d95d7f2d1ca44d2371
Zainab Nadeem Fazaa Alshubati <zainab.nadeem2015@gmail.com>
2023-08-21 23:21:28 +0300

    @zainab.nadeem
    change d

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <div class="row">
                             <div class="col-md-12">
                                 <div class="copyright">
-                                    <p>AVMS 2018. All rights reserved.</p>
+                                    <p>AVMS 2023. All rights reserved.</p>
                                 </div>
                             </div>
                         </div>
